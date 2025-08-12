5f1e312712e9919e8d21b7f679bd4652a9c654e0
david-sarra <dsarrach@gmail.com>
2025-08-12 08:13:24 +0200

    JSX Rules & Nest Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+//Section 3: React Fundamentals
+
+// JSX Rules
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <div className="someValue">
+//         <h3>hello people</h3>
+//         <ul>
+//           <li>
+//             <a href="#link">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//       <input type="text" name="" id="" />
+//     </React.Fragment>
+//   )
+// }
+
+//  Nest Components
 function Greeting() {
-    return <h2>My First Component</h2>;
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  )
+}
+
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is my message</p>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />);
+root.render(<Greeting />)
