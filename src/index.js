f4f4403ec2277b506320fa213d3c2ad7d8eca897
david-sarra <dsarrach@gmail.com>
2025-08-19 08:02:53 +0200

    Simple List

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import './index.css'
 
 //Section 3: React Fundamentals
 
+//////////
 // JSX Rules
+//////////
+
 // function Greeting() {
 //   return (
 //     <React.Fragment>
@@ -23,7 +26,10 @@ import './index.css'
 //   )
 // }
 
+//////////
 //  Nest Components
+//////////
+
 // function Greeting() {
 //   return (
 //     <div>
@@ -54,7 +60,9 @@ import './index.css'
 //   )
 // }
 
+//////////
 //Factored elements outside objects
+//////////
 
 // const Book = () => {
 //   return (
@@ -79,54 +87,97 @@ import './index.css'
 //   return <h4 style={inlineHeadingStyles}>Mel Robbins &amp; Sawyer Robbins</h4>
 // }
 
+//////////
 // Props
+//////////
 
-const firstBook = {
-  author: 'Mel Robbins &amp; Sawyer Robbins',
-  title: 'The Let Them Theory',
-  img: './images/let_them_theory.jpg',
-}
+// const firstBook = {
+//   author: 'Mel Robbins &amp; Sawyer Robbins',
+//   title: 'The Let Them Theory',
+//   img: './images/let_them_theory.jpg',
+// }
 
-const secondBook = {
-  author: 'Joël Dicker',
-  title: 'La muy catastrófica visita al zoo',
-  img: 'https://m.media-amazon.com/images/I/614Wp+ltIjL._SY342_.jpg',
-}
+// const secondBook = {
+//   author: 'Joël Dicker',
+//   title: 'La muy catastrófica visita al zoo',
+//   img: 'https://m.media-amazon.com/images/I/614Wp+ltIjL._SY342_.jpg',
+// }
+
+// const BookList = () => {
+//   return (
+//     <section className="booklist">
+//       <Book
+//         author={firstBook.author}
+//         title={firstBook.title}
+//         img={firstBook.img}
+//       >
+//         <p>
+//           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque odit,
+//           aut alias maiores voluptate deserunt non totam nulla fugiat eaque,
+//           soluta cum cupiditate impedit optio aliquam nostrum ex repudiandae
+//           nisi?
+//         </p>
+//         <button>click me</button>
+//       </Book>
+//       <Book
+//         author={secondBook.author}
+//         title={secondBook.title}
+//         img={secondBook.img}
+//       />
+//     </section>
+//   )
+// }
+
+// const Book = (props) => {
+//   const { img, title, author, children } = props
+//   console.log(props)
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//       {children}
+//     </article>
+//   )
+// }
+
+//////
+// Simple List
+/////
+
+const books = [
+  {
+    author: 'Mel Robbins &amp; Sawyer Robbins',
+    title: 'The Let Them Theory',
+    img: './images/let_them_theory.jpg',
+    id: 1,
+  },
+  {
+    author: 'Joël Dicker',
+    title: 'La muy catastrófica visita al zoo',
+    img: 'https://m.media-amazon.com/images/I/614Wp+ltIjL._SY342_.jpg',
+    id: 2,
+  },
+]
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque odit,
-          aut alias maiores voluptate deserunt non totam nulla fugiat eaque,
-          soluta cum cupiditate impedit optio aliquam nostrum ex repudiandae
-          nisi?
-        </p>
-        <button>click me</button>
-      </Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
+  const { img, title, author } = props
   console.log(props)
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
