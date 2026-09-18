815965295b47b3aa0e4d05f1b39288f440d65586
david-sarra-septeo <david.sarra@septeo.com>
2026-09-18 13:03:25 +0200

    recap first push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const campings = [
+  {
+    name: "Vilanova Park",
+    location: "Vilanova i la Geltrú",
+    img: "https://www.campingsdecatalunya.org/wp-content/uploads/vilanova-park-002-1-1024x517.jpg",
+    id: 1,
+  },
+
+  {
+    name: "Holacamp Sitges Relax",
+    location: "Sitges",
+    img: "https://www.campingsdecatalunya.org/wp-content/uploads/sitges-002-1-1024x651.jpg",
+    id: 2,
+  },
+];
+
+function CampingList() {
+  return (
+    <section className="campinglist">
+      <EventExamples/>
+      {campings.map((camping) => {
+        
+        return (
+          <Camping {...camping} key={camping.id}/>
+        );
+      })}
+    </section>);
+}
+
+const EventExamples = () => {
+  return <section>
+    <form>
+      <h2>Typical Form</h2>>
+        <input type="text" style={{ margin: '1rem 0' }} />
+        </form>;
+  </section>;
+}
+
+const Camping = (props) => {
+  const { name, location, img } = props;
+  console.log(props);
+  return (
+    <article className="camping">
+      <img src={img} alts={name} />
+      <h2>{name}</h2>
+      <h4>{location}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<CampingList />);
